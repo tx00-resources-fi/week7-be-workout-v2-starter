d1551d55d8fe135475d886ff275fe3bbfd493059
Sami <sami.bencheikh@metropolia.fi>
2024-10-04 08:00:51 +0300

    update

---
+++ tests/workout.test.js
@@ -0,0 +1,53 @@
+const mongoose = require("mongoose");
+const supertest = require("supertest");
+const app = require("../app");
+const api = supertest(app);
+const User = require("../models/userModel");
+const Workout = require("../models/workoutModel");
+const workouts = require("./data/workouts.js");
+
+let token = null;
+
+beforeAll(async () => {
+  await User.deleteMany({});
+  const result = await api
+    .post("/api/user/signup")
+    .send({ email: "mattiv@matti.fi", password: "R3g5T7#gh" });
+  token = result.body.token;
+});
+
+describe("when there is initially some workouts saved", () => {
+  beforeEach(async () => {
+    await Workout.deleteMany({});
+    await api
+      .post("/api/workouts")
+      .set("Authorization", "bearer " + token)
+      .send(workouts[0])
+      .send(workouts[1]);
+  });
+
+  test("Workouts are returned as json", async () => {
+    await api
+      .get("/api/workouts")
+      .set("Authorization", "bearer " + token)
+      .expect(200)
+      .expect("Content-Type", /application\/json/);
+  });
+
+  test("New workout added successfully", async () => {
+    const newWorkout = {
+      title: "testworkout",
+      reps: 10,
+      load: 100,
+    };
+    await api
+      .post("/api/workouts")
+      .set("Authorization", "bearer " + token)
+      .send(newWorkout)
+      .expect(201);
+  });
+});
+
+afterAll(() => {
+  mongoose.connection.close();
+});
